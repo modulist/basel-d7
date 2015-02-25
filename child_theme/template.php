0bd990579e57f02f68b75fd494c35f0166a1f58e
Claudio Luis Vera <modulist@gmail.com>
2015-02-25 12:00:38 -0500

    initial commit of sample child theme

---
+++ child_theme/template.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+//drupal_add_js ("/sites/all/themes/basel/js/jquery-ui.js");
+//drupal_add_js ("/sites/all/themes/basel/js/jquery.ui.menu.js");
+drupal_add_js ("/sites/all/themes/basel/js/basel.js");
+
