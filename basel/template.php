2539a12a778330699105f9d50671035bcf68ead3
Claudio Luis Vera <modulist@gmail.com>
2015-04-09 11:32:52 -0400

    fix parent theme grid so that regions extend to edge of page

--- basel/template.php
+++ basel/template.php
@@ -5,6 +5,4 @@
  * template.php
  */
 
-//drupal_add_js ("/sites/all/themes/basel/js/jquery-ui.js");
-//drupal_add_js ("/sites/all/themes/basel/js/jquery.ui.menu.js");
 drupal_add_js ("/sites/all/themes/basel/js/basel.js");
