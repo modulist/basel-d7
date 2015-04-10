722dc0ce7563b50433227160f2029d3699163e0f
Claudio Luis Vera <modulist@gmail.com>
2015-04-10 08:38:33 -0400

    move logo and site slogan inside of branding-first don't make the region conditional anymore

--- child_theme/templates/page.tpl.php
+++ child_theme/templates/page.tpl.php
@@ -76,42 +76,44 @@
 <div id="page-wrapper"><div id="page">
 
   <header id="header"><div class="section clearfix">
-    <?php if ($page['branding_first']): ?>
-      <div id="branding-first" class="column sidebar">
+    <div id="branding-first" class="column sidebar">
+	    <?php if ($logo): ?>
+	      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+	        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+	      </a>
+	    <?php endif; ?>
+		
+	    <?php if ($site_name || $site_slogan): ?>
+	      <div id="name-and-slogan">
+	        <?php if ($site_name): ?>
+	          <?php if ($title): ?>
+	            <div id="site-name">
+	              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+	            </div>
+	          <?php else: /* Use h1 when the content title is empty */ ?>
+	            <h1 id="site-name">
+	              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+	            </h1>
+	          <?php endif; ?>
+	        <?php endif; ?>
+	
+	        <?php if ($site_slogan): ?>
+	          <div id="site-slogan"><?php print $site_slogan; ?></div>
+	        <?php endif; ?>
+	      </div> <!-- /#name-and-slogan -->
+	    <?php endif; ?>
+	    
+	    <?php if ($page['branding_first']): ?>
         <?php print render($page['branding_first']); ?>
-      </div> <!-- /.section, /#sidebar-first -->
-    <?php endif; ?>
+	    <?php endif; ?>
+    </div> <!-- /.section, /#branding-first -->
+    
     <?php if ($page['branding_second']): ?>
       <div id="branding-second" class="column sidebar">
         <?php print render($page['branding_second']); ?>
-      </div> <!-- /.section, /#sidebar-first -->
+      </div> <!-- /.section, /#branding second -->
     <?php endif; ?>
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong></div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
 
     <?php print render($page['header']); ?>
 
